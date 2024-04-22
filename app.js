0bac8140460994dcef4ea1c3591a2f398fcd2db5
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 19:09:44 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // write code here
+function Accumulator(startingValue) {
+            this.currentValue = startingValue;
+          
+            this.read = function() {
+              let input = +prompt('Enter a number to add:', 0);
+              this.currentValue += input;
+            };
+          
+            this.show = function() {
+              alert('Current Value: ' + this.currentValue);
+            };
+          }
+          
+          let accumulator = new Accumulator(5); // กำหนด starting value เป็น 5
+          accumulator.read(); 
+          accumulator.read(); 
+          accumulator.show(); 
